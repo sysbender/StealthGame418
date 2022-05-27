ceee247c4081168091a64da21d17ab0c7b18d890
Jason <sysbender@gmail.com>
2022-05-27 10:09:03 -0400

    19 - add mission complete feedback

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -3,6 +3,10 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
 {
@@ -30,7 +34,31 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+
+
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+
+	if (MyPawn == nullptr) {
+		return;
+	}
+	// if (MyPawn && MyPawn->BIsCarryingObjective == true) {
+
+
+
 	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
+
+	if (MyPawn->BIsCarryingObjective) {
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM) {
+			GM->CompleteMission(MyPawn);
+		}
+
+	}
+	else {
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+
+
 
 }
 
