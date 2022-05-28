97cafbfd3ed9e47abce23ae511bbf4fb9112ef23
Jason <sysbender@gmail.com>
2022-05-28 12:42:59 -0400

    25 - setup hearing sense

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -13,6 +13,7 @@ AFPSAIGuard::AFPSAIGuard()
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
 
 	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnPawnSeen);
+	PawnSensingComp->OnHearNoise.AddDynamic(this,&AFPSAIGuard::OnNoiseHeard );
 
 }
 
@@ -29,7 +30,14 @@ void AFPSAIGuard::OnPawnSeen(APawn * SeenPawn)
 	if (SeenPawn == nullptr) {
 		return;
 	}
-	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Yellow, false, 10.0f);
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
+}
+
+void AFPSAIGuard::OnNoiseHeard(APawn * NoiseInstigator, const FVector & Location, float Volume)
+{
+
+
+	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, 10.0f );
 }
 
 // Called every frame
